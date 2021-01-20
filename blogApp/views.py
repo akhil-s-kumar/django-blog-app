bfe7aa3dba98846d46c37f13ca259b2416f195b0
Akhil <akhilskumar120@gmail.com>
2021-01-20 21:56:16 +0530

    pagination

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post, Categories
+from django.core.paginator import Paginator
 
 # Create your views here.
 class blog(ListView):
