a0e96fb3f0629b6bba60232cf80ed9123d093f1c
Akhil <akhilskumar120@gmail.com>
2021-01-21 14:30:12 +0530

    category view added

--- blogApp/views.py
+++ blogApp/views.py
@@ -3,6 +3,7 @@ from django.views.generic import ListView
 from .models import Post, Categories
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.http import HttpResponseNotFound, Http404
 
 # Create your views here.
 class blog(ListView):
@@ -35,3 +36,15 @@ def search(request):
    page = request.GET.get('page')
    posts = paginator.get_page(page)
    return render(request, template, {'posts':posts, 'cat_list': cat_list, 'latestpost_list':latestpost_list, 'query':query})
+
+def CategoryView(request, cats):
+   if Categories.objects.filter(categoryname=cats).exists():
+      category_posts = Post.objects.filter(category__categoryname=cats).order_by('-post_date')
+      cat_list = Categories.objects.all()
+      latestpost_list = Post.objects.all().order_by('-post_date')[:3]
+      paginator = Paginator(category_posts, 2)
+      page = request.GET.get('page')
+      category_posts = paginator.get_page(page)
+      return render(request, 'category_list.html', {'cats':cats, 'category_posts':category_posts, 'cat_list': cat_list, 'latestpost_list':latestpost_list})
+   else:
+      raise Http404
