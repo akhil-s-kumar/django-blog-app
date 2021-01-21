c4ef37c17b4c70db46f67ff9f1d0f0993e970fdc
Akhil <akhilskumar120@gmail.com>
2021-01-21 14:31:22 +0530

    category path added

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import blog, search
+from .views import blog, search, CategoryView
 
 
 urlpatterns = [
 path('', blog.as_view(), name="blog"),
 path('search', search, name="search"),
+path('category/<str:cats>/', CategoryView, name="category"),
 ]
