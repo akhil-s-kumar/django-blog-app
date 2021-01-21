7ab65816307fe83abf38e8d8f792cbebfe65f162
Akhil <akhilskumar120@gmail.com>
2021-01-21 12:32:53 +0530

    search feature added

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Post, Categories
+from django.db.models import Q
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -19,3 +20,18 @@ class blog(ListView):
       context["cat_list"] = cat_list
       context["latestpost_list"] = latestpost_list
       return context
+
+def search(request):
+   template = 'search_list.html'
+   query = request.GET.get('q')
+   if query:
+      posts = Post.objects.filter(Q(title__icontains=query) | Q(body__icontains=query)).order_by('-post_date')
+   else:
+      posts = Post.objects.all()
+   
+   cat_list = Categories.objects.all()
+   latestpost_list = Post.objects.all().order_by('-post_date')[:3]
+   paginator = Paginator(posts, 2)
+   page = request.GET.get('page')
+   posts = paginator.get_page(page)
+   return render(request, template, {'posts':posts, 'cat_list': cat_list, 'latestpost_list':latestpost_list, 'query':query})
