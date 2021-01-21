bc3a9dcf75339032c9778a7675643e75f2f04fef
Akhil <akhilskumar120@gmail.com>
2021-01-21 21:29:49 +0530

    blog detailview import and path specification

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from .views import blog, search, CategoryView
+from .views import blog, search, CategoryView, blogdetail
 
 
 urlpatterns = [
 path('', blog.as_view(), name="blog"),
 path('search', search, name="search"),
 path('category/<str:cats>/', CategoryView, name="category"),
+path('<slug:slug>/', blogdetail.as_view(), name="blog-detail"),
 ]
