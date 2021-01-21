28c388a48c0907638369590be4af4818710bc565
Akhil <akhilskumar120@gmail.com>
2021-01-21 12:31:47 +0530

    search url added

--- blogApp/urls.py
+++ blogApp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
-from .views import blog
+from .views import blog, search
 
 
 urlpatterns = [
 path('', blog.as_view(), name="blog"),
+path('search', search, name="search"),
 ]
