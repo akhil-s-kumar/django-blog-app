1cbfd6d8bdbe7942b2b51014d2acb14a9feaa170
Akhil <akhilskumar120@gmail.com>
2021-01-21 21:27:56 +0530

    url interchange

--- blog/urls.py
+++ blog/urls.py
@@ -19,8 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('blogApp.urls')),
     path('admin/', admin.site.urls),
+    path('', include('blogApp.urls')),
 ]
 
 urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
