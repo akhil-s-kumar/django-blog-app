9263177a4368fd0e534870dd4c68573e36722228
Akhil <akhilskumar120@gmail.com>
2021-01-23 23:30:44 +0530

    Slug required Field Update

--- blogApp/models.py
+++ blogApp/models.py
@@ -13,10 +13,10 @@ class Categories(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default='Blog Post')
-    slug = models.SlugField(max_length=255, null=True, blank=True)
+    slug = models.SlugField(max_length=255, null=True, blank=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     img = models.ImageField(upload_to='blog', null=True)
-    body = RichTextField(blank=True, null=True)
+    body = RichTextField(blank=False, null=True)
     post_date = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey(Categories, null=True, on_delete=models.PROTECT, related_name='category_set')
 
