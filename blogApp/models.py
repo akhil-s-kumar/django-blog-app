52a33f7afeccb13251692ae754ab6f9223efd666
Akhil <akhilskumar120@gmail.com>
2021-01-25 00:26:45 +0530

    Unique slug pre-save

--- blogApp/models.py
+++ blogApp/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, date
 from ckeditor.fields import RichTextField
+from django.db.models.signals import pre_save
+from blog.utils import unique_slug_generator
 
 # Create your models here.
 class Categories(models.Model):
@@ -13,7 +15,7 @@ class Categories(models.Model):
 class Post(models.Model):
     title = models.CharField(max_length=255)
     title_tag = models.CharField(max_length=255, default='Blog Post')
-    slug = models.SlugField(max_length=255, null=True, blank=False)
+    slug = models.SlugField(max_length=255, null=True, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     img = models.ImageField(upload_to='blog', null=True)
     body = RichTextField(blank=False, null=True)
@@ -22,3 +24,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+def slug_generator(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = unique_slug_generator(instance)
+
+pre_save.connect(slug_generator, sender=Post)
