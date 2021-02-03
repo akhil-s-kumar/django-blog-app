4d38f6d6b9a6f01d6d14f6b027ca20d23b9bd04b
Akhil <akhilskumar120@gmail.com>
2021-02-03 23:07:08 +0530

    graphene url added

--- blog/urls.py
+++ blog/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from graphene_django.views import GraphQLView
+from blog.schema import schema
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('graphql/', GraphQLView.as_view(graphiql=True)),
     path('', include('blogApp.urls')),
 ]
 
