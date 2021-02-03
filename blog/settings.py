a400406bbbc11807e5bd57752f5444b6fe534446
Akhil <akhilskumar120@gmail.com>
2021-02-03 23:06:02 +0530

    graphene update

--- blog/settings.py
+++ blog/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'graphene_django',
     'blogApp.apps.BlogappConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
 ]
+
+GRAPHENE = {
+    'SCHEMA': 'blog.schema.schema'
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
