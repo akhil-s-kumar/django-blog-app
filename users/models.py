5fac9fe7d1b6f636adaa5506ea6a8efa28dcc160
Akhil <akhilskumar120@gmail.com>
2021-02-08 23:48:09 +0530

    created profile model

---
+++ users/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.contrib.auth.models import User
+from PIL import Image
+
+# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(default='Edit your Bio!')
+    website = models.CharField(max_length=40)
